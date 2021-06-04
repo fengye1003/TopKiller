c6f4d95e0eb0a7b641a26aff790f1273b418127d
1003 fengye <mcfengye1003@outlook.com>
2021-06-04 22:34:12 +0800

    添加项目文件。

---
+++ 极刺客/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace 极刺客
+{
+    class Program
+    {
+        static void usentsd()
+        {
+            try
+            {
+                System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("StudentMain");//进程名称不要加上.exe
+                Process.Start(@".\lib\ntsd.exe", "-y -y -p " + processes[0].Id);
+            }
+            catch
+            {
+                Console.WriteLine("该程序已经被杀死");
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trying to kill TopDomain Contrler...");
+            极刺客.Program.usentsd();
+            Console.WriteLine("按任意键继续。。。code by 相互科技");
+            Console.ReadKey();
+        }
+        
+        
+    }   
+    
+}
+
---
+++ 极刺客GUI/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace 极刺客GUI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void runScript(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("StudentMain");//进程名称不要加上.exe
+                Process.Start(@".\lib\ntsd.exe", "-y -y -p " + processes[0].Id);
+                stadu.Text = "成功杀死进程";
+            }
+            catch
+            {
+                stadu.Text = "捕获进程失败";
+                MessageBox.Show("未能找到进程，似乎已经杀死该程序");
+            }
+        }
+        
+
+        
+    }
+}
